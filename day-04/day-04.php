d3f9f7db672b35190478340916c0976d94059c05
Teku <joseph.w.preston@gmail.com>
2023-12-04 01:24:37 -0500

    sleep

--- day-04/day-04.php
+++ day-04/day-04.php
@@ -51,3 +51,59 @@ foreach($winnings as $card => $total) {
 }
 
 echo "Part 1: ". $total_winnings. PHP_EOL;
+
+// Part 2
+$won = [];
+$copies = [];
+
+foreach ($lines as $i => $line) {
+
+    if($line === "") continue;
+
+    $copies = check_cards($line, $lines, $i);
+}
+
+var_dump($copies);
+
+function check_cards($line, $lines, $index, $copies = []) {
+    // card : values
+    $split_card = explode(': ', $line);
+
+    preg_match('/(\d+)/', $split_card[0], $card_info);
+    $card = $card_info[1];
+
+    $values = trim(preg_replace('/\s+/', ' ', $split_card[1]));
+
+    // winning : given values
+    $split_values = explode(' | ', $values);
+
+    $winning = explode(' ', $split_values[0]);
+    $given = explode(' ', $split_values[1]);
+
+    $draw = 0;
+
+    foreach($winning as $number) {
+        if(in_array($number, $given)) {
+            $draw += 1;// Draw +1 cards
+        }
+    }
+
+    echo "card $card draws $draw " . PHP_EOL;
+
+    // Draw cards...
+    $start_pos = $index + 1;
+
+    for($i = $start_pos; $i < $start_pos + $draw; $i++) {
+        echo $lines[$i] . PHP_EOL;
+        $copies[] = $lines[$i];
+    }
+
+    if ($i >= count($lines)) return $copies;
+
+    if ($draw > 0) {
+        echo "draw > 0 ". PHP_EOL;
+        unset($lines[$index]);
+    }
+
+    check_cards($line, $lines, $index, $copies);
+}
